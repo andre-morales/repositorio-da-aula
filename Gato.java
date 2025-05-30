9c009dcaf76203556a39d49729f2a271c186a3b4
André Morales <an.dre.m@outlook.com>
2025-05-30 09:17:03 -0300

    fiz mudanças.

--- Gato.java
+++ Gato.java
@@ -2,6 +2,8 @@ public class Gato {
 	public void mia() {
 		System.out.println("É miau!");
 		System.out.println("Miau miau.");
+	
+		System.out.println("Algumas mudanças.");
 	}
 
 	int muito_boa() {
